30606e4702d88ec0e8641d7f5f166b652ee0a4c0
potsamnang <145992304+potsamnang@users.noreply.github.com>
2024-02-08 00:08:52 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,152 @@
+import p1 from "../assets/Pic/p1.jpg";
+import p2 from "../assets/Pic/p2.jpg";
+import p3 from "../assets/Pic/p3.jpg";
+import p4 from "../assets/Pic/p4.jpg";
+import p5 from "../assets/Pic/p5.jpg";
+import p6 from "../assets/Pic/p6.jpg";
+import p7 from "../assets/Pic/p7.jpg";
+import p8 from "../assets/Pic/p8.jpg";
+import p9 from "../assets/Pic/p9.jpg";
+import p10 from "../assets/Pic/p10.jpg";
+import p11 from "../assets/Pic/p11.jpg";
+import p12 from "../assets/Pic/p12.jpg";
+import p13 from "../assets/Pic/p13.jpg";
+import p14 from "../assets/Pic/p14.jpg";
+import p15 from "../assets/Pic/p15.jpg";
+import p16 from "../assets/Pic/p16.jpg";
+import p17 from "../assets/Pic/p17.jpg";
+import p18 from "../assets/Pic/p18.jpg";
+import p19 from "../assets/Pic/p19.jpg";
+import p20 from "../assets/Pic/p20.jpg";
+import p21 from "../assets/Pic/p21.jpg";
+
+const data = [
+  {
+    id: 1,
+    img: p1,
+    name: "Pot Samnang",
+    detail: "IT",
+  },
+  {
+    id: 2,
+    img: p2,
+    name: "Sok Chandara",
+    detail: "Teacher",
+  },
+  {
+    id: 3,
+    img: p3,
+    name: "Chea Sokha",
+    detail: "IT Support",
+  },
+  {
+    id: 4,
+    img: p4,
+    name: "Dina Ro",
+    detail: "Manager",
+  },
+  {
+    id: 5,
+    img: p5,
+    name: "Phon Ponnak",
+    detail: "Driver",
+  },
+  {
+    id: 6,
+    img: p6,
+    name: "Toch Sochan",
+    detail: "Saler",
+  },
+  {
+    id: 7,
+    img: p7,
+    name: "Heng Chanheanh",
+    detail: "Designer",
+  },
+  {
+    id: 8,
+    img: p8,
+    name: "Massas",
+    detail: "Driver",
+  },
+  {
+    id: 9,
+    img: p9,
+    name: "Hok Lihor",
+    detail: "Teacher",
+  },
+  {
+    id: 10,
+    img: p10,
+    name: "Aok Vanna",
+    detail: "Student",
+  },
+  {
+    id: 11,
+    img: p11,
+    name: "Annita",
+    detail: "Manager",
+  },
+  {
+    id: 12,
+    img: p12,
+    name: "Chet Chanpisey",
+    detail: "Lower",
+  },
+  {
+    id: 13,
+    img: p13,
+    name: "Channa",
+    detail: "Supporter",
+  },
+  {
+    id: 14,
+    img: p14,
+    name: "Theary",
+    detail: "Student",
+  },
+  {
+    id: 15,
+    img: p15,
+    name: "Srey Moew",
+    detail: "Saler",
+  },
+  {
+    id: 16,
+    img: p16,
+    name: "Rin Maneth",
+    detail: "IT",
+  },
+  {
+    id: 17,
+    img: p17,
+    name: "Sothyuth",
+    detail: "IT",
+  },
+  {
+    id: 18,
+    img: p18,
+    name: "Sok San",
+    detail: "Teacher",
+  },
+  {
+    id: 19,
+    img: p19,
+    name: "Korey",
+    detail: "Driver",
+  },
+  {
+    id: 20,
+    img: p20,
+    name: "Eng Phearun",
+    detail: "Saler",
+  },
+  {
+    id: 21,
+    img: p21,
+    name: "Seak Chanmoney",
+    detail: "IT",
+  },
+];
+
+export default data;
